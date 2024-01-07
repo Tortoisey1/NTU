94e2e474058d505e82affa838c85c4af14071f05
Iz_Goh <88613481+Tortoisey1@users.noreply.github.com>
2024-01-07 17:50:28 +0800

    Delete SC1003/Python/LAB 2 directory

--- SC1003/Python/LAB 2/Discussion 4 cracked version.py
+++
@@ -1,9 +0,0 @@
-User_number = int(input("Please enter pattern width: "))
-
-for number in range (2 * User_number):
-        if number < 5:
-            print (number * "*",end="")
-            print()
-        else:
-            print ( (10 - number)*"*",end="")
-            print()
